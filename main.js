965bf6878e7c871776bf9c417ec7e1c1f9df4285
Emma Gale <emmajgale@hotmail.vom>
2021-02-20 20:12:31 +0000

    sorting out the on off functionality of the share button

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ const active = document.getElementById("active");
 
 function toggleActive() {
   console.log("trying to toggle");
-  if ((active.style.visibility = "hidden")) {
+
+  if (active.style.visibility === "hidden") {
     active.style.visibility = "visible";
   } else {
     active.style.visibility = "hidden";
   }
 }
+
+share.addEventListener("click", toggleActive);
 
 share.addEventListener("click", toggleActive);
 
