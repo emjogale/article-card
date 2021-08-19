5541f981caf9865712e09712dd143c9cfa0275fe
Emma <48389529+emjogale@users.noreply.github.com>
2021-08-19 07:22:05 +0100

    change javascript function to toggle classlist rather than changing the style in the script

--- main.js
+++ main.js
@@ -4,21 +4,9 @@ const active = document.getElementById("active");
 const imgActive = document.getElementById("activeShare");
 
 function toggleActive() {
-  console.log("trying to toggle");
-
-  if (active.style.visibility === "hidden") {
-    active.style.visibility = "visible";
-  } else {
-    active.style.visibility = "hidden";
-  }
+	active.classList.toggle("notHidden");
 }
 
 share.addEventListener("click", toggleActive);
 
 imgActive.addEventListener("click", toggleActive);
-
-// share.addEventListener("click", function () {
-//   if (this.classList.contains("active")) {
-//     this.classList.remove("active");
-//   } else this.classList.add("active");
-// });
